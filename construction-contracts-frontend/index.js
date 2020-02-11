31a75a37410a55bfb7fc9faf015be0c3aceacade
figueroar <raul.a.figueroa@hotmail.com>
2020-02-11 09:50:17 -0500

    can add contract/project without reload

--- construction-contracts-frontend/index.js
+++ construction-contracts-frontend/index.js
@@ -1,12 +1,47 @@
 const BASE_URL = "http://localhost:3000"
-
 window.addEventListener('load', () => {
     showContractors()
     //console.log('DOM fully loaded and parsed');
-});
+    headerEventListeners();
+    
 
+});
+///////////Listeners///////
+function headerEventListeners(){
+
+        document.getElementById("links").innerHTML += `<ul>
+        <button class="allContractors" >All contractors</button> 
+        <button class="displayContractorForm" > New Contractor</button>
+        <button class="allProjects" > All projects</button>
+        </ul> 
+        `
+    
+    let showContractors = document.querySelector("button.allContractors")
+        showContractors.addEventListener("click", (e) => {
+            console.log("e",e )
+            clearForm();
+        })
+
+    let displayContractForm = document.querySelector("button.displayContractorForm")
+        displayContractForm.addEventListener("click", (e) => {
+            console.log("e",e )
+            displayContractorForms();
+        })
+
+    let allProjects = document.querySelector("button.allProjects")
+        allProjects.addEventListener("click", (e) => {
+            console.log("e",e )
+            allProjects();
+        })
+}
+
+// function attachClickToContractors(){
+    
+// }
+
+///////////Project//////////////////
 function allProjects(){
-    //clearForm();
+    clearForm();
     let main = document.getElementById("main-form")
     fetch("http://localhost:3000/contracts")
     .then(resp => resp.json())
@@ -18,11 +53,8 @@ function allProjects(){
         </li>
         `).join('')
     })
-
 }
-
 function removeProject(id){
-
     fetch(BASE_URL + `/contracts/${id}`, {
         method: "DELETE",
         headers: {
@@ -30,13 +62,11 @@ function removeProject(id){
             'Accept': 'application/json'
         }
     })
-
 }
-
-
 
 function assignProject(e){
     //clearForm();
+    e.preventDefault();
     let main = document.getElementById("main-form")
     let html = `
     <form onsubmit="createContract();return false;">
@@ -66,9 +96,8 @@ function assignProject(e){
     <input type="number" id="Months Overdue"></br>
     <label>Staff Total:</label>
     <input type="number" id="Staff Total"></br>
-
     <label>Contractor has hidden field Id:</label>
-    <input type="hidden" id="contractorID" value="${e}" data-id="${e}"> </br>
+    <input type="hidden" id="contractorID" value="${e.currentTarget.dataset.id}" data-id="${e.currentTarget.dataset.id}"> </br>
     
     <label>Complete:</label>
     <input type ="checkbox" id="Project Completed"></br>
@@ -76,7 +105,6 @@ function assignProject(e){
 `
     main.innerHTML = html
 }
-
 function createContract(){
     contract = {
         projectName: document.getElementById("Project Name").value,
@@ -95,7 +123,6 @@ function createContract(){
         monthsOverDue: document.getElementById("Months Overdue").value,
         contractor_id: document.getElementById("contractorID").value
     }
-
     fetch("http://localhost:3000/contracts", {
         method: "POST",
         body: JSON.stringify(contract),
@@ -104,12 +131,55 @@ function createContract(){
             'Accept': 'application/json'
         }
     })
+    clearForm();
 }
 
 
 
+///////////Contractor//////////////////////////
+function showContractors(){
+    //console.log("i think its working")
+    //clearForm();
+    let main = document.getElementById("main")
+    main.innerHTML = "<h2>Contractors</h2>"
+    fetch("http://localhost:3000/contractors")
+    .then(resp => resp.json())
+    .then(contractors => {
+        main.innerHTML+= contractors.map(contractor =>  `
+        <li><a href="#" data-id="${contractor.id}">${contractor.lastName}</a> 
+        <button data-id=${contractor.id} class="delete" ; return false;>Delete</button>
+        <button data-id=${contractor.id} class="edit" ; return false;>Edit</button>
+        <button data-id=${contractor.id} class="contract" ; return false;>Assign project</button>
+        `).join('');
 
+        let newContract = document.querySelectorAll("button.contract")
+            newContract.forEach( assignProjectButton => {
+                assignProjectButton.addEventListener("click", (e) => {
+                    console.log(e.currentTarget.dataset.id)
+                    assignProject(e)
+                })
+            })
 
+        let editContractor = document.querySelectorAll("button.edit")
+            editContractor.forEach( editButton => {
+                editButton.addEventListener("click", (e) => {
+                    console.log(e.currentTarget.dataset.id)
+                    //editContractor(e)
+                    
+                })
+            })
+        
+        let deleteContractor = document.querySelector("button.delete")
+            deleteContractor.forEach( deleteButton => {
+                deleteButton.addEventListener("click", (e) => {
+                    console.log(e.currentTarget.dataset.id)
+                    //removeContractor(e)
+                })
+        })
+    })
+    
+    
+}
 function removeContractor(id){
     //console.log("e", e)
     //clearForm();
@@ -120,41 +190,18 @@ function removeContractor(id){
             'Accept': 'application/json'
         }
     })
-    preventDefault();
-    showContractors();
+    .then(showContractors);
 }
-
 function editContractor(e){
     //clearForm();
     console.log(e)
 }
-
-function showContractors(){
-    //console.log("i think its working")
-    //clearForm();
-    let main = document.getElementById("main")
-    main.innerHTML = "<h2>Contractors</h2>"
-
-    fetch("http://localhost:3000/contractors")
-    .then(resp => resp.json())
-    .then(contractors => {
-        main.innerHTML+= contractors.map(contractor =>  `
-        <li><a href="#" data-id="${contractor.id}">${contractor.lastName}</a> 
-        <button data-id=${contractor.id} class="delete" onclick="removeContractor(${contractor.id})"; return false;>Delete</button>
-        <button data-id=${contractor.id} class="edit" onclick="editContractor(${contractor.id})"; return false;>Edit</button>
-        <button data-id=${contractor.id} class="contract" onclick="assignProject(${contractor.id})"; return false;>Assign project</button>
-        </li>
-        `).join('')
-    })
-    
-}
-
 function displayContractorForms() {
     //clearForm();
     let main = document.getElementById("main-form")
     
     let html = `
-        <form onsubmit="createContractor();return false;">
+        <form>
         <label>First Name:</label>
         <input type ="text" id="First Name"></br>
         <label>Last Name:</label>
@@ -169,14 +216,20 @@ function displayContractorForms() {
         <input type ="text" id="City"></br>
         <label>Country:</label>
         <input type ="text" id="Country"></br>
-        <input type ="submit" value="Create Contractor">
+        <input type ="submit" class="CreateContractor"value="Create Contractor">
     `
     main.innerHTML = html
-    
-}
+    let executeContractor = document.querySelector("input.CreateContractor")
+    executeContractor.addEventListener("click", (e) => {
+        console.log(e)
+        e.preventDefault();
+        createContractor();
+    })
 
+}
 function createContractor(){
-    
+
+    let main = document.getElementById("main")
     user = {
         firstName: document.getElementById("First Name").value,
         lastName: document.getElementById("Last Name").value,
@@ -186,7 +239,6 @@ function createContractor(){
         city: document.getElementById("City").value,
         country: document.getElementById("Country").value
     }
-
     fetch(BASE_URL+'/contractors',{
         method: "POST",
         body: JSON.stringify(user),
@@ -196,25 +248,21 @@ function createContractor(){
         }
     })
     .then(resp => resp.json())
-    .then(contractors => {
-        main.innerHTML+= contractors.map(contractor =>  `
+    .then(contractor => {
+        main.innerHTML+= `
         <li><a href="#" data-id="${contractor.id}">${contractor.lastName}</a> 
-        <button data-id=${contractor.id} class="delete" onclick="removeContractor(${contractor.id})"; return false;>Delete</button>
-        <button data-id=${contractor.id} class="edit" onclick="editContractor(${contractor.id})"; return false;>Edit</button>
-        <button data-id=${contractor.id} class="contract" onclick="assignProject(${contractor.id})"; return false;>Assign project</button>
+        <button data-id=${contractor.id} class="delete"; return false;>Delete</button>
+        <button data-id=${contractor.id} class="edit" ; return false;>Edit</button>
+        <button data-id=${contractor.id} class="contract" ; return false;>Assign project</button>
         </li>
-        `).join('')
+        `
+    clearForm();
     })
+    
 }
 
-
-
-// function clearForm(){
-//    let main = document.getElementById("main-form")
-//    main.innerHTML = ""
-// }
-
-
-
-
-
+///////Clear Forum///////
+function clearForm(){
+    let main = document.getElementById("main-form")
+    main.innerHTML = ""
+ }
