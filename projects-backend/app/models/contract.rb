864cb66d85b83435ed02ed4d9e926bcd4ee315d0
figueroar <raul.a.figueroa@hotmail.com>
2020-02-16 14:22:04 -0500

    validations added

--- projects-backend/app/models/contract.rb
+++ projects-backend/app/models/contract.rb
@@ -1,3 +1,19 @@
 class Contract < ActiveRecord::Base
     belongs_to :contractor
+
+
+    validates :projectName, presence: :true
+    validates :projectStreet, presence: :true
+    validates :projectCity, presence: :true 
+    validates :projectCountry, presence: :true 
+    validates :projectBudget, presence: :true 
+    validates :projectBeginDate, presence: :true 
+    validates :projectEndDate,  presence: :true
+    validates :projectType, presence: :true 
+    validates :projectInformation, presence: :true 
+    validates :projectStaff,  presence: :true
+    validates :projectCompleted, presence: :true 
+    validates :monthsEstimated, presence: :true 
+    validates :monthsCurrent, presence: :true 
+    validates :monthsOverDue,  presence: :true
 end
--- projects-backend/app/models/contractor.rb
+++ projects-backend/app/models/contractor.rb
@@ -1,3 +1,11 @@
 class Contractor < ActiveRecord::Base
     has_many :contracts
+
+    validates :firstName, presence: :true
+    validates :lastName, presence: :true
+    validates :phoneNum, presence: :true
+    validates :email, presence: :true
+    validates :companyName, presence: :true
+    validates :city, presence: :true
+    validates :country, presence: :true
 end
