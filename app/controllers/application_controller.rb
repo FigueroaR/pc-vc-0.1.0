26895f964d8b861830a6e11def78d6c1fd906c65
figueroar <raul.a.figueroa@hotmail.com>
2020-03-26 05:30:05 -0400

    action cont file render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
     def index
-      render file: './projects-frontend/index.html'
+      render file: 'projects-frontend/index.html'
     end
 end
