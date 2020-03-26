22395355198b06be88b5565319a5beb7085b8bc7
figueroar <raul.a.figueroa@hotmail.com>
2020-03-26 05:27:04 -0400

    applciation controller updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::API
-  class ApplicationController < ActionController::API
     def index
       render file: './projects-frontend/index.html'
     end
-  end
 end
