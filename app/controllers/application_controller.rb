66423c795ff79283872424a45e97fd315b9951a2
figueroar <raul.a.figueroa@hotmail.com>
2020-03-26 05:25:31 -0400

    route to index html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::API
+  class ApplicationController < ActionController::API
+    def index
+      render file: './projects-frontend/index.html'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :contracts
   resources :contractors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  match '*all', to: 'application#index', via: [:get]
 end
