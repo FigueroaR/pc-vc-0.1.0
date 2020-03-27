08c5f2e2de0c7e87079700f44a99998e9db8446b
figueroar <raul.a.figueroa@hotmail.com>
2020-03-26 21:52:49 -0400

    root added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :contracts
   resources :contractors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  match '*all', to: 'welcome#index', via: [:get]
+  # match '*all', to: 'welcome#index', via: [:get]
+  root :to => 'welcome#index'
 end
