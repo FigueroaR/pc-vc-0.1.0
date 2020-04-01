bda124b25101c79ac904596fd119874ef3ee9581
figueroar <raul.a.figueroa@hotmail.com>
2020-04-01 18:46:24 -0400

    seed working on test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 person1 = Contractor.create(firstName: 'Paul' ,lastName: 'Jones', phoneNum: 1234567890, email: 'paul@jones.com', companyName: 'Jones & Co', city: 'Newark, NJ', country: 'USA')
-person2 = Contractor.create(firstName: 'Lucas',lastName: 'West', phoneNum: 2345678901, email: 'lucas@west.com', companyName: 'West & East', city: 'San Diego, CA', country: 'USA')
-person3 = Contractor.create(firstName: 'Eddie',lastName: 'Murphy', phoneNum: 4567891230, email: 'eddie@murphy.com', companyName: 'Alaugh & Co', city: 'Los Angeles, CA', country: 'USA')
-person4 = Contractor.create(firstName: 'George',lastName: 'Lopez', phoneNum: 7894561230, email: 'george@lopez.com', companyName: 'Lopez & Co', city: 'Los Angeles,CA', country: 'USA')
+person2 = Contractor.create(firstName: 'Lucas',lastName: 'West', phoneNum: 1345678901, email: 'lucas@fakemail.com', companyName: 'West & East', city: 'San Diego, CA', country: 'USA')
+person3 = Contractor.create(firstName: 'Eddie',lastName: 'Murphy', phoneNum: 1567891230, email: 'eddie@fakemail.com', companyName: 'Alaugh & Co', city: 'Los Angeles, CA', country: 'USA')
+person4 = Contractor.create(firstName: 'George',lastName: 'Lopez', phoneNum: 1894561230, email: 'george@fakemail.com', companyName: 'Lopez & Co', city: 'Los Angeles,CA', country: 'USA')
